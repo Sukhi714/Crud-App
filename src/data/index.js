56da56d245f02d4d656c23f927440d11afb0230a
Sukhdeep <sukhdeepsinghsembhi5911@gmail.com>
2024-04-10 12:59:25 +0530

    files added

---
+++ src/data/index.js
@@ -0,0 +1,13 @@
+ const employeesData = [
+  {
+    id: 1,
+    firstName: 'sukhi',
+    lastName: 'sembhi',
+    email: 'sukhiii@gmail.com',
+    salary: '6,423,764',
+    date: '2024-04-04',
+  }
+
+ ];
+
+ export { employeesData };
